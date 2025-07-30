d2be56b7237b0d2dc36522aba03a180bd1116f8e
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2025-07-30 15:34:48 +0700

    feat: add initial Tabler project files and configuration

--- build.ps1
+++ build.ps1
@@ -1,8 +1,21 @@
 dotnet --version
-dotnet clean   .\src\Core -c Release -tl
-dotnet restore .\src\Core
-dotnet build   .\src\Core -c Release -tl
 
-dotnet clean   .\tests\ -c Release -tl
-dotnet restore .\tests\
-dotnet build   .\tests\ -c Release -tl
+# Build Core library
+dotnet clean   ./src/Core/Wangkanai.Tabler.csproj -c Release -tl
+dotnet restore ./src/Core/Wangkanai.Tabler.csproj
+dotnet build   ./src/Core/Wangkanai.Tabler.csproj -c Release -tl
+
+# Build Components library
+dotnet clean   ./src/Components/Wangkanai.Tabler.Components.csproj -c Release -tl
+dotnet restore ./src/Components/Wangkanai.Tabler.Components.csproj
+dotnet build   ./src/Components/Wangkanai.Tabler.Components.csproj -c Release -tl
+
+# Build Web Components library
+dotnet clean   ./src/Web/Wangkanai.Tabler.Components.Web.csproj -c Release -tl
+dotnet restore ./src/Web/Wangkanai.Tabler.Components.Web.csproj
+dotnet build   ./src/Web/Wangkanai.Tabler.Components.Web.csproj -c Release -tl
+
+# Build Tests
+dotnet clean   ./tests/Wangkanai.Tabler.Tests.csproj -c Release -tl
+dotnet restore ./tests/Wangkanai.Tabler.Tests.csproj
+dotnet build   ./tests/Wangkanai.Tabler.Tests.csproj -c Release -tl
